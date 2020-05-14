266194878ed63df3d6a63d273eac7fdecc360ec6
panzertatata <ta200037@gmail.com>
2020-05-14 10:44:32 +0700

    navbar complete70%

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <b-navbar toggleable="lg" type="dark" variant="dark">
+      <!-- <b-navbar toggleable="lg" type="dark" variant="dark">
     <b-navbar-brand >Online shopping</b-navbar-brand>
     <router-link to="/main">
     <b-button size="sm" class="mb-2" variant="outline-info">
@@ -26,7 +26,23 @@
 
       </b-navbar-nav>
     </b-collapse>
-  </b-navbar>  
+  </b-navbar>   -->
+  <b-navbar toggleable="sm" type="dark" variant="dark">
+    <b-navbar-brand href="/#/main">Online Shopping</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="/#/signin" class="nav-item">| Sign in</b-nav-item>
+        <b-nav-item href="/#/signup" class="nav-item">| Sign up</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
    </div>
     <router-view/>
   </div>
@@ -39,6 +55,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.nav-item{
+  font-weight: bold;
+  color: white;
+  font-size: 15px;
+}
+
+.nav-item:hover{
+  background-color: grey;
+  transition: 0.5s;
 }
 
 #nav a {
